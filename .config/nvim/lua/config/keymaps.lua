3049a37709bb54ae5603e79527cc1bc30ee4416a
umaidashi <umaidashi18@gmail.com>
2024-03-17 22:56:44 +0900

    feat: add keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "<c-h>", "<Left>")
+vim.keymap.set("i", "<c-j>", "<Down>")
+vim.keymap.set("i", "<c-k>", "<Up>")
+vim.keymap.set("i", "<c-l>", "<Right>")
