bc95de2e61388ea9c20daad614406d3a4b71b3a0
umaidashi <umaidashi18@gmail.com>
2024-03-17 21:29:52 +0900

    feat: add .p10k.zsh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dotfiles=(.zshrc .tmux.conf .vimrc .wezterm.lua .config)
+dotfiles=(.zshrc .tmux.conf .vimrc .wezterm.lua .p10k.zsh .config)
 
 for file in "${dotfiles[@]}"; do
-        ln -svf ~/dotfiles/$file ~/
+	ln -svf ~/dotfiles/$file ~/
 done
