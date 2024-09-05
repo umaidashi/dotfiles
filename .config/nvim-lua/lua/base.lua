dbaff2a96cabd09120a232a564267fe2d1aa6f23
umaidashi <umaidashi18@gmail.com>
2024-09-05 09:11:49 +0900

    feat: add opt

--- .config/nvim-lua/lua/base.lua
+++ .config/nvim-lua/lua/base.lua
@@ -1,1 +1,12 @@
 vim.wo.number = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+
+vim.opt.cursorline = true
+vim.opt.showmatch = true
+vim.opt.showmode = false
+vim.opt.showcmd = true
+vim.opt.showtabline = 2
+vim.opt.showbreak = "↪"
