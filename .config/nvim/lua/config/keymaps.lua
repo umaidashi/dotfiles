ff67e9d7cc6889a78df252befd890a24698bcf4b
umaidashi <umaidashi18@gmail.com>
2024-09-07 23:54:52 +0900

    feat: add keymaps

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,23 @@
+local map = vim.keymap.set
+
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+map({ "n", "x" }, "J", "10j", { desc = "Down 10" })
+map({ "n", "x" }, "K", "10k", { desc = "Up 10" })
+
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+map({ "n", "x" }, "qq", "<cmd>qa!<cr>", { desc = "Quit" })
+map({ "n", "x" }, "wq", "<cmd>wqa!<cr>", { desc = "Save and Quit" })
+map({ "n", "x" }, "<leader>w", "<cmd>w<cr>", { desc = "Save" })
