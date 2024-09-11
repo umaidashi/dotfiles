bcb66f6b884511edefe936370fe2b3cc252ae43f
umaidashi <umaidashi18@gmail.com>
2024-09-11 15:00:58 +0900

    feat: autosave autocmds

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,20 @@
+-- autosave
+local function save()
+  local buf = vim.api.nvim_get_current_buf()
+
+  vim.api.nvim_buf_call(buf, function()
+    vim.cmd("silent! write")
+  end)
+end
+
+vim.api.nvim_create_augroup("AutoSave", {
+  clear = true,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  callback = function()
+    save()
+  end,
+  pattern = "*",
+  group = "AutoSave",
+})
