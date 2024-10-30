3c7ef979a531e3203fc7509a8b1aad4215bd81ce
umaidashi <umaidashi18@gmail.com>
2024-10-30 22:12:12 +0900

    fix: renmae

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,43 +1,44 @@
-local map = vim.keymap.set
+local set = vim.keymap.set
+local del = vim.keymap.del
 local Util = require("lazyvim.util")
 
-map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+set({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+set({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
-map({ "n", "x" }, "J", "10j", { desc = "Down 10" })
-map({ "n", "x" }, "K", "10k", { desc = "Up 10" })
+set({ "n", "x" }, "J", "10j", { desc = "Down 10" })
+set({ "n", "x" }, "K", "10k", { desc = "Up 10" })
 
-map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
-map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
-map({ "n", "x" }, "qq", "<cmd>qa!<cr>", { desc = "Quit" })
-map({ "n", "x" }, "wq", "<cmd>wqa!<cr>", { desc = "Save and Quit" })
-map({ "n", "x" }, "<leader>w", "<cmd>w<cr>", { desc = "Save" })
+set({ "n", "x" }, "qq", "<cmd>qa!<cr>", { desc = "Quit" })
+set({ "n", "x" }, "wq", "<cmd>wqa!<cr>", { desc = "Save and Quit" })
+set({ "n", "x" }, "<leader>w", "<cmd>w<cr>", { desc = "Save" })
 
-map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+set("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+set("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 
-map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
-map("i", "<C-p>", "<Up>", { desc = "Up" })
-map("i", "<C-n>", "<Down>", { desc = "Down" })
-map("i", "<C-f>", "<Right>", { desc = "Right" })
-map("i", "<C-b>", "<Left>", { desc = "Left" })
+set("i", "<C-p>", "<Up>", { desc = "Up" })
+set("i", "<C-n>", "<Down>", { desc = "Down" })
+set("i", "<C-f>", "<Right>", { desc = "Right" })
+set("i", "<C-b>", "<Left>", { desc = "Left" })
 
 -- ターミナルにボーダーを追加する。
 local lazyterm = function()
   Util.terminal(nil, { cwd = Util.root(), border = "rounded" })
 end
-map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
-map("n", "<C-/>", lazyterm, { desc = "Terminal (root dir)" })
+set("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
+set("n", "<C-/>", lazyterm, { desc = "Terminal (root dir)" })
