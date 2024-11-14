4c625c13756a1dd01e21c2a11b1dd9bbe8dbb0ec
umaidashi <umaidashi18@gmail.com>
2024-11-14 15:34:54 +0900

    fix: lint

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,9 +14,9 @@ config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
 config.colors = {
-  tab_bar = {
-    inactive_tab_edge = "none",
-  },
+	tab_bar = {
+		inactive_tab_edge = "none",
+	},
 }
 
 -- Font
