597cd3e4a35b8f1894bf329a82f82872b3d2a64e
umaidashi <umaidashi18@gmail.com>
2024-12-24 11:24:17 +0900

    feat: del unused plugins

--- .config/nvim/lua/plugins/disable.lua
+++ .config/nvim/lua/plugins/disable.lua
@@ -4,7 +4,6 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/ts-comments.nvim", enabled = false },
   { "echasnovski/mini.ai", enabled = false },
-  { "folke/lazydev.nvim", enabled = false },
   { "Bilal2453/luvit-meta", enabled = false },
   { "folke/flash.nvim", enabled = false },
 }
