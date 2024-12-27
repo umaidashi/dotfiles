b753a58478819c45c54cc05a36523a481b3e6082
umaidashi <umaidashi18@gmail.com>
2024-12-27 17:31:46 +0900

    feat: add git plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,1 +1,11 @@
 require("full-border"):setup()
+require("git"):setup()
+
+THEME.git = THEME.git or {}
+THEME.git.added = ui.Style():fg("green")
+THEME.git.modified = ui.Style():fg("blue")
+THEME.git.deleted = ui.Style():fg("red"):bold()
+THEME.git = THEME.git or {}
+THEME.git.added_sign = "A"
+THEME.git.modified_sign = "M"
+THEME.git.deleted_sign = "D"
